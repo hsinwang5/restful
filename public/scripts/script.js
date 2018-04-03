c29296c49cb997d4e39d1cad4689e00f4bbdb6b6
Hsin Wang <hsinwang5@gmail.com>
2018-04-02 21:16:40 -0400

    initial add

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+console.log("connected");
+
+var test = document.querySelector("#delete");
+if (test){
+    test.addEventListener("click", function(){
+        console.log("clicked this button!");
+    });
+}
+
